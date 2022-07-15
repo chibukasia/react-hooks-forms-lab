e05fd15dbd337f4bdf0d24044c8c3e0b8ab0f69b
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-07-15 18:28:14 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,33 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  // const [itemName, setItemName] = useState("");
+  // const [itemCategory, setItemCategory] = useState("Produce");
+
+  // const newItem = {
+  //   id: uuid(), 
+  //   name: itemName,
+  //   category: itemCategory
+
+  // }
+
+  // const newDataArray = [...items, newItem]
+  
+  // function handleNewItemName(e){
+  //   setItemName(e.target.value);
+  // }
+
+  // function handleNewItemCategory(e){
+  //   setItemCategory(e.target.value);
+  // }
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
-
+// // newly added
+//   function handleNewItem(){
+//     setItems()
+//   }
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,27 +2,62 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+  const [itemsData, setItemsData] = useState(items)
+
+  
+  
+  function handleNewItemName(e){
+    setItemName(e.target.value);
+  }
+
+  function handleNewItemCategory(e){
+    setItemCategory(e.target.value);
+  }
+
+  function handleAddNewItem(e){
+
+    e.preventDefault();
+    const newItem = {
+      id: uuid(), 
+      name: itemName,
+      category: itemCategory
+  
+    }
+  
+    const newDataArray = [...itemsData, newItem];
+    setItemsData(newDataArray);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearch(e){
+    setSearch(e.target.value);
+  }
+  const itemsToDisplay = itemsData
+  .filter((item)=> item.name.includes(search))
+  .filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  })
+  
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onNewItemName={handleNewItemName} onNewItemCategoryName ={handleNewItemCategory} onItemFormSubmit={handleAddNewItem}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearch} search={search}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} name={item.name} category={item.category} search={search}/>
         ))}
       </ul>
     </div>
